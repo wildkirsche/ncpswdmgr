bb976eff5a4ec2b1075ad1432140d83a0e53bc71
wildkirsche <wildkirsche@quantentunnel.de>
2014-04-27 17:18:07 +0200

    Added methods to save and load passwords
    Next step is to add some kind of error handling.

--- pswdDB.py
+++ pswdDB.py
@@ -19,3 +19,17 @@ def loadDB(path):
 
 def saveDB(path, DB):
 	pickle.dump((DB.DB, DB.mpswd), open(path, "wb"))
+
+def addpswd(DB, site, login, pswd, mpswd):
+	if crypto.hash(mpswd) = DB.mpswd:
+		DB.DB[site] = (login, crypto.encrypt(pswd, mpswd))
+		return "WIN"
+	else:
+		return "FAIL"
+
+def getpswd(DB, site, mpswd):
+	if crypto.hash(mpswd) = DB.mpswd:
+		login , pswd = DB.DB[site]
+		return (login, crypto.decrypt(pswd, mpswd))
+	else:
+		return "FAIL"
